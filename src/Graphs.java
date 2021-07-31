ab084d00528cde55f1c4ee317e5ef4593463ac41
Soham Mukherjee <rockstarsam1234@gmail.com>
2021-07-31 19:01:10 +0530

    Graph codes

--- src/Graphs.java
+++ src/Graphs.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class Graphs {
     ArrayList<Integer> []graph;
@@ -42,3 +41,77 @@ public class Graphs {
         }
     }
 }
+
+class Cell{
+    int row;
+    int col;
+
+    Cell(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
+
+
+class MatrixGraph{
+
+    private int [][]mat;
+    private int N, M;
+    int [][]distance;
+
+    MatrixGraph(int [][]mat){
+        this.mat = new int[mat.length][mat[0].length];
+        distance = new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat.length;j++){
+                this.mat[i][j] = mat[i][j];
+                distance[i][j] = -1;
+            }
+
+        }
+        N = mat.length;
+        M = mat[0].length;
+    }
+
+    private boolean isValidCell(int row, int col){
+        if(row < 0 || row >= N || col < 0 || col >= M || mat[row][col] == 0){
+            return false;
+        }
+
+        return true;
+    }
+
+    private void bfs(){
+        Queue<Cell> q = new LinkedList<Cell>();
+        q.add(new Cell(0, 0));
+        List<List<Integer>> moves;
+        moves = Arrays.asList(Arrays.asList(1, 0), Arrays.asList(0, 1), Arrays.asList(-1, 0), Arrays.asList(0, -1));
+        distance[0][0] = 0;
+        while(!q.isEmpty()){
+            Cell curr = q.poll();
+            for(List<Integer> move: moves){
+                int nextRow = curr.row + move.get(0);
+                int nextCol = curr.col + move.get(1);
+                if(isValidCell(nextRow, nextCol) && distance[nextRow][nextCol] == -1){
+                    distance[nextRow][nextCol] = distance[curr.row][curr.col] + 1;
+                    q.add(new Cell(nextRow, nextCol));
+                }
+            }
+
+        }
+    }
+
+    public int getLeastDistance(){
+        bfs();
+        return distance[N-1][M-1];
+    }
+
+}
+
+/*
+*     src = 231
+*     destination = 0
+*      +1, -1, /2
+
+* /
+ */
